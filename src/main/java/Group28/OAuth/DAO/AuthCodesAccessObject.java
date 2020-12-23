189027d3b908d72b76820ab119ca8824b195610d
Szymon Lepianka <slepianka@wp.pl>
2020-12-23 02:25:01 +0100

    Added context of AuthCode to auth_codes column

--- src/main/java/Group28/OAuth/DAO/AuthCodesAccessObject.java
+++ src/main/java/Group28/OAuth/DAO/AuthCodesAccessObject.java
@@ -20,7 +20,8 @@ public class AuthCodesAccessObject implements IDataBaseAccessObject<AuthCode>{
     private AuthCode createAuthCodeFromResult(ResultSet rs) throws SQLException {
         AuthCode authCode = new AuthCode();
         authCode.setId(rs.getLong("auth_code_id"));
-        authCode.setExpiresAt(rs.getDate("expires_at"));
+        authCode.setExpiresAt(rs.getTimestamp("expires_at"));
+        authCode.setContent(rs.getString("content"));
         authCode.setRevoked(rs.getBoolean("revoked"));
         ClientApp clientApp = DatabaseEditor.getInstance().getAppsAccessObject().readById(rs.getLong("client_app_id"));
         authCode.setClientApp(clientApp);
@@ -53,26 +54,28 @@ public class AuthCodesAccessObject implements IDataBaseAccessObject<AuthCode>{
 
     @Override
     public AuthCode create(AuthCode object) throws SQLException {
-        String query = "INSERT INTO auth_codes (expires_at, revoked, client_app_id, user_id)" + "values (?,?,?,?)";
+        String query = "INSERT INTO auth_codes (expires_at, revoked, content, client_app_id, user_id)" + "values (?,?,?,?,?)";
         PreparedStatement preparedStmt = this.connection.prepareStatement(query);
-        preparedStmt.setDate(1, object.getExpiresAt());
+        preparedStmt.setTimestamp(1, object.getExpiresAt());
         preparedStmt.setBoolean(2, object.isRevoked());
-        preparedStmt.setLong(3, object.getClientApp().getId());
-        preparedStmt.setLong(4, object.getUser().getId());
+        preparedStmt.setString(3, object.getContent());
+        preparedStmt.setLong(4, object.getClientApp().getId());
+        preparedStmt.setLong(5, object.getUser().getId());
         preparedStmt.execute();
         return object;
     }
 
     @Override
     public AuthCode update(AuthCode object) throws SQLException {
-        String query = "UPDATE auth_codes SET expires_at = ?, revoked = ?, client_app_id = ?, user_id = ? WHERE auth_code_id = ?";
+        String query = "UPDATE auth_codes SET expires_at = ?, revoked = ?, content = ?, client_app_id = ?, user_id = ? WHERE auth_code_id = ?";
         PreparedStatement preparedStmt = this.connection.prepareStatement(query);
-        preparedStmt.setDate(1, object.getExpiresAt());
+        preparedStmt.setTimestamp(1, object.getExpiresAt());
         preparedStmt.setBoolean(2, object.isRevoked());
-        preparedStmt.setLong(3, object.getClientApp().getId());
-        preparedStmt.setLong(4, object.getUser().getId());
+        preparedStmt.setString(3,object.getContent());
+        preparedStmt.setLong(4, object.getClientApp().getId());
+        preparedStmt.setLong(5, object.getUser().getId());
         //where
-        preparedStmt.setLong(5, object.getId());
+        preparedStmt.setLong(6, object.getId());
         preparedStmt.execute();
         return object;
     }
