ce6e0d084b97bbcc45fd28f6a7828e186ca110a1
Bartlomiej Kregielewski <bartlomiejkregielewski@gmail.com>
2021-01-03 19:18:00 +0100

    Login functionality

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
+INSERT INTO scopes (scope_id, name) VALUES (1, "user_email"), (2, "user_username"), (3, "user_firstname"), (4, "user_surname"), (5, "user_phonenumber"), (6, "user_birthdate");
 INSERT INTO users (user_id, email, is_developer, password, username) VALUES (1,"admin@localhost", true, "$2a$10$/a7SLmCEgOFwkxUNGwnOYe4e6PkyZT58n3GAeKiXe5IvXwDn7vouC", "admin");
 INSERT INTO client_apps (client_app_id, app_secret, redirecturl, user_id, age_restriction) VALUES (1,111111,"localhost",1, 0);
